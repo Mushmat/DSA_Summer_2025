7cc4eb5b309ffe72506c88e335c0615af3e8436d
Mushmat <edu.chirayu2005@gmail.com>
2025-05-16 09:43:24 +0530

    Completed for and while loops

---
+++ Basics_of_C++/For_While_Loops.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    for(int i = 0; i < 10; i++){
+        cout << "Hi" << " " << i << endl;
+    }
+
+    int i = 0;
+    while(i < 10){
+        cout << "Hi" << " " << i << endl;
+        i++;
+    }
+
+    //If we put the while code into a do-while code, the code will execute for at least one time, irrespective of the condition, because
+    //the code checks the 'do' first then it checks 'while'
+    return 0;
+}
